894e187d5421ea34d6b73cb5a2545c778e235c7a
Evan Johnson <e5johnso@eng.ucsd.edu>
2024-06-11 11:47:50 -0700

    make nueral nets cloneable

--- src/nn.rs
+++ src/nn.rs
@@ -32,6 +32,7 @@ use crate::regularization::Regularization;
 ///    assert_eq!(nn.forward(&[1., 0.]).first().unwrap().round(), 1.);
 ///    assert_eq!(nn.forward(&[1., 1.]).first().unwrap().round(), 0.);
 ///```
+#[derive(Clone)]
 pub struct NN {
     weights: Vec<Array2<f32>>, // layer * 2D matrix e.g. [2x2], [2x1]
     bias: Vec<Array2<f32>>,    //layer * 2D matrix
--- src/regularization.rs
+++ src/regularization.rs
@@ -1,5 +1,5 @@
 use std::fmt::Display;
-
+#[derive(Clone)]
 pub enum Regularization {
     None,
     L1(f32),
